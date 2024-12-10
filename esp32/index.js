7f38640f1d1f731506792748aa9507e1243a4083
Gabriel-Savoldi <gabrielbaldassarini@gmail.com>
2024-12-09 21:32:27 -0300

    teste30

--- esp32/index.js
+++ esp32/index.js
@@ -50,7 +50,7 @@ function obterDataAtual() {
   ano + 10;
 
   // Criar um novo objeto Date com os valores ajustados
-  const novaData = new Date(ano + 5, mes - 1, dia);  // O mês precisa ser ajustado para a base 0
+  const novaData = new Date(ano + 1, mes - 1, dia);  // O mês precisa ser ajustado para a base 0
 
   // Recuperando a data ajustada
   const novoDia = String(novaData.getDate()).padStart(2, '0'); // Recupera o novo dia
